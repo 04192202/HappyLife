b8347dfa24bda46886cd3c2567452db20b526e5b
Hwan <845033564@qq.com>
2023-01-06 12:13:35 +0800

    Initial Commit

---
+++ LittlePink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink
+//
+//  Created by 郝义鹏 on 2023/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
